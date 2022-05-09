911cfea7ca53c0843479dd8dbbe15996b9d798d7
Sib.MH <chengjie.michael.huang@gmail.com>
2022-05-09 12:58:10 -0700

    added error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,28 @@
 // add bootstrap css 
 // import 'bootstrap/dist/css/bootstrap.css'
+
 import '@styles/globals.css'
 import Layout from '../components/layout'
+import ErrorBoundary from '../components/ErrorBoundary'
 
-let src="/vecteezy_animation-of-fog-or-smoke-moving-on-white-background-closeup-shot_1797524.mov"
+let src = "/vecteezy_animation-of-fog-or-smoke-moving-on-white-background-closeup-shot_1797524.mov"
 // let src="/white_smoke.mp4"
 
 function Application({ Component, pageProps }) {
   return (
     <div className='main'>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <video autoPlay muted loop className='video'>
-        <source src={src} type="video/mp4"/>
-        Your browser does not support the video tag or format
-      </video>
-    </div>
+      <ErrorBoundary
+      // FallbackComponent={ErrorFallback}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <video autoPlay muted loop className='video'>
+          <source src={src} type="video/mp4" />
+          Your browser does not support the video tag or format
+        </video>
+      </ErrorBoundary>
+    </div >
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ export default function Home() {
         <h1 className='big_text'> As an: </h1>
         <a className='centered medium_text'>🦝 adventure seeker 🦝 <br></br>🥔 extroverted introvert 🥔<br></br> 🤓 engineering and computer science graduate 🤓</a>
         <br></br>
-        <a className='centered big_text'> I am passionate about exploring novel environments, experiencing new occurences, and improving accessibility through automation. <br></br><br></br> Hit me up if you think we have chemistry!</a>
-      </div>    </div>
+        <a className='centered big_text'> I am interested in exploring novel environments, experiencing new occurences, and improving accessibility through automation. <br></br><br></br> Hit me up if you think we have chemistry!</a>
+      </div>
+    </div>
   )
 }
