51055bcab402d420be77feeb97cce72d96f0bb8f
Sib.MH <chengjie.michael.huang@gmail.com>
2022-05-09 14:11:58 -0700

    re-added error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,22 @@
 
 import '@styles/globals.css'
 import Layout from '../components/layout'
-// import ErrorBoundary from '../components/ErrorBoundary'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 let src = "/vecteezy_animation-of-fog-or-smoke-moving-on-white-background-closeup-shot_1797524.mov"
-// let src="/white_smoke.mp4"
 
 function Application({ Component, pageProps }) {
   return (
     <div className='main'>
       <Layout>
-        {/* <ErrorBoundary> */}
+        <ErrorBoundary>
           <Component {...pageProps} />
-        </Layout>
-        <video autoPlay muted loop className='video'>
-          <source src={src} type="video/mp4" />
-          Your browser does not support the video tag or format
-        </video>
-      {/* </ErrorBoundary> */}
+        </ErrorBoundary>
+      </Layout>
+      <video autoPlay muted loop className='video'>
+        <source src={src} type="video/mp4" />
+        Your browser does not support the video tag or format
+      </video>
     </div >
   )
 }
