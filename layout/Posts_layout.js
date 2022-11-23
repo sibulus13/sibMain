95a8c63a2f635693d727e08e46a5a6f31c314ff1
Sib <chengjie.michael.huang@gmail.com>
2022-11-22 21:34:02 -0800

    styled

--- layout/Posts_layout.js
+++ layout/Posts_layout.js
@@ -6,6 +6,7 @@ import Filter from "components/Filter";
 import styles from "./Posts.module.css";
 import { filter_include_tags, getDates, getTags } from "lib/utils";
 
+// Displays posts in column on main page
 export default function Posts_layout({ posts, href_prefix, title }) {
   // console.log(posts)
   let tags = getTags(posts);
@@ -17,56 +18,47 @@ export default function Posts_layout({ posts, href_prefix, title }) {
   );
 
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <h1 className={styles.normal_color}> {title} </h1>
-        {/* <br className={[styles.normal_color, styles.break_line]}></br> */}
-      </div>
-      <div className={styles.post_column}>
-        <Filter tags={tags} setTags={setCheckedState.bind(this)}></Filter>
-        {posts.map((post, index) => {
-          if (
-            post.frontMatter.published &&
-            filter_include_tags(post.frontMatter.tags, tags, checkedState)
-          ) {
-            return (
-              <Link href={href_prefix + post.slug} passHref key={index}>
-                <div className={styles.post_container}>
-                  <div className={styles.post_text}>
-                    <p className={styles.post_title}>
-                      {post.frontMatter.title}
-                    </p>
-                    <hr className={styles.post_break_line}></hr>
-                    <p className={styles.post_description}>
-                      {post.frontMatter.description}
-                    </p>
-                    <p className={styles.post_date}>
-                      <small className={styles.post_date}>
-                        {post.frontMatter.last_updated_date ||
-                          post.frontMatter.date}
-                      </small>
-                    </p>
-                  </div>
-                  <div className={styles.post_img_container}>
-                    {post.frontMatter.thumbnailUrl && (
-                      <Image
-                        src={post.frontMatter.thumbnailUrl}
-                        alt="thumbnail"
-                        quality={100}
-                        layout="fill"
-                        objectFit="scale-down"
-                        // className={}
-                        // width="100%"
-                        // height="100%"
-                      />
-                    )}
-                  </div>
+    <div className={styles.post_column}>
+      <Filter tags={tags} setTags={setCheckedState.bind(this)}></Filter>
+      {posts.map((post, index) => {
+        if (
+          post.frontMatter.published &&
+          filter_include_tags(post.frontMatter.tags, tags, checkedState)
+        ) {
+          return (
+            <Link href={href_prefix + post.slug} passHref key={index}>
+              <div className={styles.post_container}>
+                <div className={styles.post_text}>
+                  <p className={styles.post_title}>
+                    {post.frontMatter.title}
+                  </p>
+                  <hr className={styles.post_break_line}></hr>
+                  <p className={styles.post_description}>
+                    {post.frontMatter.description}
+                  </p>
+                  <p className={styles.post_date}>
+                    <small className={styles.post_date}>
+                      {post.frontMatter.last_updated_date ||
+                        post.frontMatter.date}
+                    </small>
+                  </p>
                 </div>
-              </Link>
-            );
-          }
-        })}
-      </div>
+                <div className={styles.post_img_container}>
+                  {post.frontMatter.thumbnailUrl && (
+                    <Image
+                      src={post.frontMatter.thumbnailUrl}
+                      alt="thumbnail"
+                      quality={100}
+                      layout="fill"
+                      objectFit="scale-down"
+                    />
+                  )}
+                </div>
+              </div>
+            </Link>
+          );
+        }
+      })}
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,36 +4,44 @@ import "@styles/globals.css";
 import Layout from "../components/layout";
 import ErrorBoundary from "../components/ErrorBoundary";
 import { useRef } from "react";
+import { variants } from "lib/constants";
+import styles from "./App.module.css";
+import { labelMapper } from "lib/utils";
+
 // import { MDXProvider } from "@mdx-js/react";
 
 let src =
   "/vecteezy_animation-of-fog-or-smoke-moving-on-white-background-closeup-shot_1797524.mov";
-const variants = {
-  hidden: { opacity: 0, x: 0, y: -50 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: 100 },
-};
 
-function Application({ Component, pageProps, router }) {
-  // console.log(router.pathname);
-  // console.log('router.route', router.route)
-  // console.log(pageProps)
+function Application(props) {
   // console.log(pageProps.type)
+  console.log(`isMobileView`, props.isMobileView)
+  let Component = props.Component
+  let pageProps = props.pageProps
   const videoRef = useRef();
   const setPlayBack = () => {
-    console.log(`setting playback`);
     videoRef.current.playbackRate = 1;
   };
+
   return (
-      <div className="main">
-        <Layout>
-          <ErrorBoundary>
+    <div className="main">
+      <Layout>
+        <ErrorBoundary>
+          <motion.main>
+            <motion.div
+              className={props.isMobileView?styles.title_m:styles.title}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: "linear", duration: 1 }} // Set the transition to linear
+            >
+              <h1 className={styles.normal_color}> {labelMapper(pageProps.type)} </h1>
+            </motion.div>
             <AnimatePresence
               exitBeforeEnter
-              // onExitComplete={() => window.scrollTo(0, 0)}
               initial={true}
             >
-              <motion.main
+              <motion.div
                 key={pageProps.type}
                 variants={variants} // Pass the variant object into Framer Motion
                 initial="hidden" // Set the initial state to variants.hidden
@@ -42,24 +50,37 @@ function Application({ Component, pageProps, router }) {
                 transition={{ type: "linear", duration: 1 }} // Set the transition to linear
               >
                 <Component {...pageProps} />
-              </motion.main>
+              </motion.div>
             </AnimatePresence>
-          </ErrorBoundary>
-        </Layout>
-        <video
-          autoPlay
-          muted
-          loop
-          className="video"
-          ref={videoRef}
-          onCanPlay={() => setPlayBack()}
-          on
-        >
-          <source src={src} type="video/mp4" />
-          Your browser does not support the video tag or format
-        </video>
-      </div>
+          </motion.main>
+        </ErrorBoundary>
+      </Layout>
+      <video
+        autoPlay
+        muted
+        loop
+        className="video"
+        ref={videoRef}
+        onCanPlay={() => setPlayBack()}
+      >
+        <source src={src} type="video/mp4" />
+        Your browser does not support the video tag or format
+      </video>
+    </div>
+
   );
 }
 
 export default Application;
+
+Application.getInitialProps = async ({ ctx }) => {
+  let isMobileView = (ctx.req
+    ? ctx.req.headers['user-agent']
+    : navigator.userAgent).match(
+      /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i
+    )
+  //Returning the isMobileView as a prop to the component for further use.
+  return {
+    isMobileView: Boolean(isMobileView)
+  }
+}
